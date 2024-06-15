8055dd95d47705d2a9ae958783410c741bdb1124
binnmti <binnmtiisprogrammer@gmail.com>
2024-06-15 23:01:30 +0900

    Add Maui

---
+++ NostrClientCSharp/NostrClientCSharp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NostrClientCSharp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
